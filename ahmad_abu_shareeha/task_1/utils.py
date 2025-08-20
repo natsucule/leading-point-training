c9d4da57f3a10370ac8da0b56d0ff9afcfb57c9a
Ahmad Abushareeha <ahmadshareeha@gmail.com>
2025-08-20 12:11:52 +0300

    fix code

--- ahmad_abu_shareeha/task_1/utils.py
+++ ahmad_abu_shareeha/task_1/utils.py
@@ -1,5 +1,5 @@
 import json
-
+from mimetypes import read_mime_types
 
 todo_file = "todo.json"
 
@@ -9,7 +9,7 @@ try:
     with open(todo_file, "r") as file:
         notes = json.load(file)
 except (FileNotFoundError, json.JSONDecodeError):
-    pass
+    notes = []
 
 # Save updated notes back to file
 def save_notes():
@@ -57,6 +57,3 @@ def list_all():
         print(f"{i}. {note}")
 
     print("Notes:")
-
-
-
